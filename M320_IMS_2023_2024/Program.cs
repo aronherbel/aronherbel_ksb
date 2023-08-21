1aa0dec68c711f5b6870a06e913f4d9e0cf59c87
Aron Herbel <aron.herbel@ksb-sg.ch>
2023-08-21 10:22:11 +0200

    Add project files.

---
+++ M320_IMS_2023_2024/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace M320_IMS_2023_2024
+{
+    internal class Program
+    {
+        int zahl1 = 15;
+        string name = "Aron Herbel";
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
