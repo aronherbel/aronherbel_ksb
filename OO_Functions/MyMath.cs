cb3096a2c8369bcdd67fe24705050ae0e7f93d86
Aron Herbel <aron.herbel@ksb-sg.ch>
2023-08-23 22:27:05 +0200

    version1.0

---
+++ OO_Functions/MyMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO_Functions
+
+{
+    public static class MyMath
+    {
+        public static int Calc_ggt(int numOne, int numTwo)
+        {
+            while (numTwo != 0)
+            {
+                int temp = numTwo;
+                numTwo = numOne % numTwo;
+                numOne = temp;
+            }
+            return numOne;
+        }
+
+        public static int Calc_kgv(int numOne, int numTwo)
+        {
+            int max = Math.Max(numOne, numTwo);
+            int kgv = max;
+
+            while (true)
+            {
+                if (kgv % numOne == 0 && kgv % numTwo == 0)
+                    return kgv;
+
+                kgv += max;
+            }
+        }
+        
+    }
+}
--- OO_Functions/Program.cs
+++ OO_Functions/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Was möchten sie berechnen ggT[1]/kgv[2]");
+            string auswahl1 = Console.ReadLine();
+            int auswahl = int.Parse(auswahl1);
+
+            switch (auswahl)
+            {
+                case 1:
+                    Console.WriteLine("Geben Sie bitte ihre erste Zahl ein");
+                    string a_ggt = Console.ReadLine();
+                    Console.WriteLine("Geben Sie bitte ihre zweite Zahl ein");
+                    string b_ggt = Console.ReadLine();
+
+                    int numOne_ggt = int.Parse(a_ggt);
+                    int numTwo_ggt = int.Parse(b_ggt);
+
+                    int result_ggt = MyMath.Calc_ggt(numOne_ggt, numTwo_ggt);
+
+                    Console.WriteLine("Der ggT beträgt " + result_ggt);
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("Geben Sie bitte ihre erste Zahl ein");
+                    string a_kgv = Console.ReadLine();
+                    Console.WriteLine("Geben Sie bitte ihre zweite Zahl ein");
+                    string b_kgv = Console.ReadLine();
+
+                    int numOne_kgv = int.Parse(a_kgv);
+                    int numTwo_kgv = int.Parse(b_kgv);
+
+                    int result_kgv = MyMath.Calc_kgv(numOne_kgv, numTwo_kgv);
+
+                    Console.WriteLine("Der kgv beträgt " + result_kgv);
+
+                    break;
+            }
+
+            
+
         }
     }
 }
