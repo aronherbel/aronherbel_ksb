22093d03997c1ec922bb229edaf7548674f0bc09
Aron Herbel <aron.herbel@ksb-sg.ch>
2023-08-27 23:47:52 +0200

    Version 2.0

--- OO_Functions/MyMath.cs
+++ OO_Functions/MyMath.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,17 +26,103 @@ namespace OO_Functions
 
         public static int Calc_kgv(int numOne, int numTwo)
         {
-            int max = Math.Max(numOne, numTwo);
-            int kgv = max;
+            int ggt = Calc_ggt(numOne, numTwo);
+            return (numOne * numTwo) / ggt;
+        }
 
+        public static int ReadInt()
+        {
             while (true)
             {
-                if (kgv % numOne == 0 && kgv % numTwo == 0)
-                    return kgv;
-
-                kgv += max;
+                Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+                if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Ihre Zahl ist ungültig \n beachten sie folgende Regeln \n 1. Positiv und Ganzzahlig \t 2. Nicht 0");
+                }
             }
         }
-        
+
+        public static void ShowResult(string operation, int numOne, int numTwo, int result)
+        {
+            Console.WriteLine($"{operation} von {numOne} und {numTwo} ist {result}\n\n");
+            
+        }
+
+        public static int Calc_ggt_r(int numOne, int numTwo)
+        {
+            if (numTwo == 0)
+            {
+                return numOne;
+            }
+            else
+            {
+                return Calc_ggt(numTwo, numOne % numTwo); 
+            }
+            
+        }
+
+        public static double Calc_Mittelwert(int[] numbers)
+        {
+            double sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum / numbers.Length;
+        }
+
+        public static int Calc_KleinsterWert(int[] numbers)
+        {
+            int a = int.MaxValue;
+            foreach (int num in numbers)
+            {
+                if (num < a)
+                {
+                    a = num;
+                }
+            }
+            return a;
+        }
+
+        public static int Calc_GroessterWert(int[] numbers)
+        {
+            int a = int.MinValue;
+            foreach (int num in numbers)
+            {
+                if (num > a)
+                {
+                    a = num;
+                }
+            }
+            return a;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void ReverseArray(int[] array)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left < right)
+            {
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+
+                left++;
+                right--;
+            }
+            
+        }
     }
 }
--- OO_Functions/Program.cs
+++ OO_Functions/Program.cs
@@ -1,48 +1,127 @@
-﻿namespace OO_Functions
+﻿
+using System.Linq.Expressions;
+
+namespace OO_Functions
 {
     internal class Program
     {
+        static int[] ReadNumbersFromConsole(int count)
+        {
+            int[] numbers = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Bitte geben Sie Zahl {i + 1} ein:");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            return numbers;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Was möchten sie berechnen ggT[1]/kgv[2]");
-            string auswahl1 = Console.ReadLine();
-            int auswahl = int.Parse(auswahl1);
-
-            switch (auswahl)
-            {
-                case 1:
-                    Console.WriteLine("Geben Sie bitte ihre erste Zahl ein");
-                    string a_ggt = Console.ReadLine();
-                    Console.WriteLine("Geben Sie bitte ihre zweite Zahl ein");
-                    string b_ggt = Console.ReadLine();
-
-                    int numOne_ggt = int.Parse(a_ggt);
-                    int numTwo_ggt = int.Parse(b_ggt);
-
-                    int result_ggt = MyMath.Calc_ggt(numOne_ggt, numTwo_ggt);
-
-                    Console.WriteLine("Der ggT beträgt " + result_ggt);
-
-                    break;
-
-                case 2:
-                    Console.WriteLine("Geben Sie bitte ihre erste Zahl ein");
-                    string a_kgv = Console.ReadLine();
-                    Console.WriteLine("Geben Sie bitte ihre zweite Zahl ein");
-                    string b_kgv = Console.ReadLine();
-
-                    int numOne_kgv = int.Parse(a_kgv);
-                    int numTwo_kgv = int.Parse(b_kgv);
-
-                    int result_kgv = MyMath.Calc_kgv(numOne_kgv, numTwo_kgv);
-
-                    Console.WriteLine("Der kgv beträgt " + result_kgv);
-
-                    break;
-            }
-
             
+
+            do
+            {
+                
+                Console.WriteLine("\nWas möchten sie berechnen ggT[1]\nkgv[2]\nggT_r[3]\nmehrere Zahlen verwenden[4]");
+                string auswahl1 = Console.ReadLine();
+                int auswahl = int.Parse(auswahl1);
+
+
+                switch (auswahl)
+                {
+                    case 1:
+                        string operation_ggt = "ggt";
+                        int numOne_ggt = MyMath.ReadInt();
+                        int numTwo_ggt = MyMath.ReadInt();
+
+                        int result_ggt = MyMath.Calc_ggt(numOne_ggt, numTwo_ggt);
+
+                        MyMath.ShowResult(operation_ggt, numOne_ggt, numTwo_ggt, result_ggt);
+
+                        Console.WriteLine("Wollen sie die Zahlen tauschen?\nJa[1]\nNein[2]");
+                        string wahl1 = Console.ReadLine();
+                        int wahl = int.Parse(wahl1);
+
+                        switch (wahl) 
+                        { 
+                            case 1:
+                                //Hier erhalte ich Fehler
+                                //int umgekehrt = MyMath.Swap(ref numOne_ggt, ref numTwo_ggt);
+                                break;
+                            case 2:
+                            Console.WriteLine("Oke");
+                                break;
+                        }
+
+                        break;
+
+                    case 2:
+                        string operation_kgv = "kgv";
+                        int numOne_kgv = MyMath.ReadInt();
+                        int numTwo_kgv = MyMath.ReadInt();
+
+                        int result_kgv = MyMath.Calc_kgv(numOne_kgv, numTwo_kgv);
+
+                        MyMath.ShowResult(operation_kgv, numOne_kgv, numTwo_kgv, result_kgv);
+
+                        break;
+
+                    case 3:
+                        string operation_ggtr = "ggt_r";
+                        int numOne_ggtr = MyMath.ReadInt();
+                        int numTwo_ggtr = MyMath.ReadInt();
+
+                        int result_ggtr = MyMath.Calc_kgv(numOne_ggtr, numTwo_ggtr);
+
+                        MyMath.ShowResult(operation_ggtr, numOne_ggtr, numTwo_ggtr, result_ggtr);
+
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Geben sie die länge ihres zahles ein");
+                        string count1 = Console.ReadLine();
+                        int count = int.Parse(count1);
+
+                        int[] numbers = Program.ReadNumbersFromConsole(count);
+
+                        Console.WriteLine("Was möchtest du berechnen Mittelwert[1]\nkleinste Zahl[2]\ngrösste Zahl[3]\numkehren[4]");
+                        string auswahl2 = Console.ReadLine();
+                        int auswahl3 = int.Parse(auswahl2);
+
+                        switch (auswahl3)
+                        {
+                            case 1:
+                                double mittelwert = MyMath.Calc_Mittelwert(numbers);
+                                Console.WriteLine($"Resultat beträgt {mittelwert}");
+                                break;
+
+                            case 2:
+                                int kleinste = MyMath.Calc_KleinsterWert(numbers);
+                                Console.WriteLine($"Resultat beträgt {kleinste}");
+                                break;
+
+                            case 3:
+                                double biggest = MyMath.Calc_GroessterWert(numbers);
+                                Console.WriteLine($"Resultat beträgt {biggest}");
+                                break;
+                                
+                                case 4:
+                                // hier erhalt ich fehler:
+                                // double reverse = MyMath.ReverseArray(numbers);
+                                Console.WriteLine($"Resultat beträgt {reverse}");
+                                break;
+                        }
+
+                        break;
+                }
+            } while (true);
+
+
+
+
+
 
         }
     }
 }
+
